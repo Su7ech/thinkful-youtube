0c7b534cf240ae0ac7e4edb268da5a5bf6180a6b
Su7ech <tpbaroody@gmail.com>
2015-09-15 13:55:54 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$(function () {
+    $('#search-term').submit(function (event) {
+        event.preventDefault();
+        $('hr').show();
+        var userInput = $(event.target).children('[type=text]').val();
+        getRequest(userInput);
+    })
+});
+
+function getRequest(query) {
+    var params = {
+        part: 'snippet',
+        q: query,
+        maxResults: 20,
+        key: 'AIzaSyDuJn991RJ55MWCO8Uv5HPkzH0-iqp3ups'
+    }
+    url = 'https://www.googleapis.com/youtube/v3/search';
+
+    $.getJSON(url, params, function(data) {
+        // displayResults(data.items.title);
+        displayResults(data.items);
+    });
+}
+
+function displayResults (data) {
+    var html = "";
+    $.each(data, function (i, item) {
+        var videoTitle = item.snippet.title;
+        var videoId = item.id.videoId;
+        // console.log(videoTitle);
+        // console.log(videoId);
+
+        html += "<li><p><span>" + videoTitle + "</span></p><iframe width='200px' src='https://www.youtube.com/embed/" + videoId + "'/></li>";
+        // $('#search-results ul').prepend("<li><p><span>" + videoTitle + "</span></p></li>");
+    });
+    $('#search-results ul').html(html);
+}
